f2ecdd8bb50d0b95351eb50ef6c5e36b30e7a364
cmhill <cmhill@umd.edu>
2011-10-11 03:42:34 -0400

    Added scripts to run ButtonTest (still need to run xcode twice).

--- modules/gui-model-iph/resources/user-scripts/iph-sample-workflow.sh
+++ modules/gui-model-iph/resources/user-scripts/iph-sample-workflow.sh
@@ -10,24 +10,25 @@ SCRIPT_DIR=`dirname $0`
 
 #------------------------
 # application directory 
-aut_dir=$SCRIPT_DIR/jfc-aut/RadioButton/
+aut_dir=$SCRIPT_DIR/iph-aut/ButtonTest
 
 # application classpath 
 aut_classpath=$aut_dir/bin
 
 # server host name
-aut_serverhost="localhost"
+#aut_serverhost="localhost"
 
 # server port number
 port=8082
 
 # application main class
-mainclass="Project"
+mainclass="ButtonTest"
 
 # Change the following 2 lines for the classpath and the main class of your 
 # application. The example is for CrosswordSage, another real world example
 # in the jfc-aut directory (http://crosswordsage.sourceforge.net/)
 
+#aut_classpath=$SCRIPT_DIR/iph-aut/ButtonTest/
 #aut_classpath=$SCRIPT_DIR/jfc-aut/CrosswordSage/bin:$SCRIPT_DIR/jfc-aut/CrosswordSage/bin/CrosswordSage.jar
 #mainclass="crosswordsage.MainScreen"
 
@@ -43,17 +44,17 @@ configuration="$aut_dir/guitar-config/configuration.xml"
 
 # intial waiting time
 # change this if your application need more time to start
-intial_wait=1000
+intial_wait=100
 
 # delay time between two events during ripping 
-ripper_delay=500
+ripper_delay=100
 
 # the length of test suite
 tc_length=2
 
 # delay time between two events during replaying  
 # this number is generally smaller than the $ripper_delay
-relayer_delay=200
+relayer_delay=1000
 
 #------------------------
 # Output artifacts 
@@ -97,7 +98,8 @@ echo ""
 echo "About to rip the application " 
 read -p "Press ENTER to continue..."
 #cmd="$SCRIPT_DIR/jfc-ripper.sh -cp $aut_classpath -c $mainclass -g  $gui_file -cf $configuration -d $ripper_delay -i $intial_wait -l $log_file"
-cmd="$SCRIPT_DIR/iph-ripper.sh -cp $aut_classpath -sh $aut_serverhost -p $port"
+# Moved -sp localhost
+cmd="$SCRIPT_DIR/iph-ripper.sh -cp $aut_classpath -p $port -g  $gui_file -cf $configuration -d $ripper_delay -i $intial_wait -l $log_file"
 
 # Adding application arguments if needed 
 if [ ! -z $args ] 
@@ -108,8 +110,8 @@ echo $cmd
 eval $cmd
 
 # Converting GUI structure to EFG
-echo ""
-echo "About to convert GUI structure file to Event Flow Graph (EFG) file" 
+#echo ""
+#echo "About to convert GUI structure file to Event Flow Graph (EFG) file" 
 read -p "Press ENTER to continue..."
 cmd="$SCRIPT_DIR/gui2efg.sh $gui_file $efg_file"
 echo $cmd
@@ -119,7 +121,7 @@ eval $cmd
 echo ""
 echo "About to generate test cases to cover all possible $tc_length-way event interactions" 
 read -p "Press ENTER to continue..."
-cmd="$SCRIPT_DIR/tc-gen-sq.sh $efg_file $tc_length 0 $testcases_dir"
+cmd="$SCRIPT_DIR/tc-gen-sq.sh -e $efg_file -l $tc_length -m 0 -d $testcases_dir"
 echo $cmd
 eval $cmd 	
 # Replaying generated test cases
@@ -127,21 +129,22 @@ echo ""
 echo "About to replay test case(s)" 
 echo "Enter the number of test case(s): "
 read testcase_num
-
+    
 for testcase in `find $testcases_dir -name "*.tst"| head -n$testcase_num`  
-do
-	# getting test name 
-	test_name=`basename $testcase`
-	test_name=${test_name%.*}
-
-	cmd="$SCRIPT_DIR/jfc-replayer.sh -cp $aut_classpath -c  $mainclass -g $gui_file -e $efg_file -t $testcase -i $intial_wait -d $relayer_delay -l $logs_dir/$test_name.log -gs $states_dir/$test_name.sta -cf $configuration"
-
-	# adding application arguments if needed 
-	if [ ! -z $args ] 
-	then 
-		cmd="$cmd -a \"$args\" " 
-	fi	
-	echo $cmd 
-	eval $cmd
+    do
+    # getting test name 
+   	test_name=`basename $testcase`
+   	test_name=${test_name%.*}
+   
+    # Removed -c
+    
+   	cmd="$SCRIPT_DIR/iph-replayer.sh -cp $aut_classpath -g $gui_file -e $efg_file -t $testcase -i $intial_wait -d $relayer_delay -so 1000 -l $logs_dir/$test_name.log -gs $states_dir/$test_name.sta -cf $SCRIPT_DIR/configuration.xml"
+   	# adding application arguments if needed 
+   	if [ ! -z $args ] 
+   	then 
+   		cmd="$cmd -a \"$args\" " 
+   	fi	
+   	echo $cmd 
+   	eval $cmd
 done
 
--- modules/gui-replayer-iph/resources/user-scripts/iph-replayer.sh
+++ modules/gui-replayer-iph/resources/user-scripts/iph-replayer.sh
@@ -19,59 +19,54 @@
 #  THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 
 ####################################################
-# JFC ripper launching script
+# IPH replayer launching script
 #
 #	By	baonn@cs.umd.edu
 #	Date: 	06/08/2011
 ####################################################
 
-
 function usage {
-	echo "Usage: iphripper.sh -cp <aut classpath> -p <port> [guitar arguments]"
+	echo "Usage: $0 -cp <aut classpath> [guitar arguments]"
 }
 
 base_dir=`dirname $0`
 guitar_lib=$base_dir/jars
-
 
 if [ $# -lt 2 ] 
 then
 	echo "Missing parameter(s)"
 	usage
 	exit
+else	
+	if [ $1 == "-cp" ]
+	then
+		addtional_classpath=$2
+	else
+		echo "Invalid parameter(s)"
+		usage
+		exit
+		
+	fi
 fi
-
-if [ $1 == "-cp" ]
-then
-	addtional_classpath=$2
-else
-	echo "Invalid parameter(s)"
-	usage
-	exit
-	
-fi
-
 
 guitar_args=${@:3}
 
-classpath=$addtional_classpath:$classpath
+classpath=$addtional_classpath:$classpath:$base_dir
 
 # Main classes 
-ripper_launcher=edu.umd.cs.guitar.ripper.IphRipperMain
+replayer_launcher=edu.umd.cs.guitar.replayer.IphReplayerMain
 
 
 for file in `find $guitar_lib/ -name "*.jar"`
 do
-    guitar_classpath=${file}:${guitar_classpath}
+guitar_classpath=${file}:${guitar_classpath}
 done
-
 
 # Change GUITAR_OPTS variable to run with the clean log file  
 GUITAR_OPTS="$GUITAR_OPTS -Dlog4j.configuration=log/guitar-clean.glc"
 
 if [ -z "$JAVA_CMD_PREFIX" ];
 then
-    # Run with clean log file 
     JAVA_CMD_PREFIX="java"
 fi
 
@@ -81,9 +76,15 @@ if [ ! -z $addtional_classpath ]
 then
 	classpath=$classpath:$addtional_classpath
 else
-	classpath=$classpath
+	classpath=$classpath.
 fi
 
-RIPPER_CMD="$JAVA_CMD_PREFIX $GUITAR_OPTS -cp $classpath $ripper_launcher $guitar_args"
-exec $RIPPER_CMD
+REPLAYER_CMD="$JAVA_CMD_PREFIX $GUITAR_OPTS -cp $classpath $replayer_launcher $guitar_args"
+
+#echo $REPLAYER_CMD
+exec $REPLAYER_CMD
+
+
+
+
 
